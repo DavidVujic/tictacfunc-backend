41a836f8ce42dabce3779ed2bf5646597e825e0d
davidvujic <david@vujic.se>
2016-07-26 01:13:08 +0200

    consume aws lambda function

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var https = require('https');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
-    res.status(200).send('Hello, world!');
+    var game = req.query.game;
+    var callback = req.query.callback;
+
+    var options = {
+        hostname: 'f11xvgecf1.execute-api.us-west-2.amazonaws.com',
+        port: 443,
+        path: '/prod/play?game=' + game + '&callback=' + callback,
+        method: 'GET'
+    };
+
+    var httpReq = https.request(options, function (httpRes) {
+        httpRes.on('data', function (d) {
+            res.jsonp(d);
+        });
+    });
+    httpReq.end();
+
+    httpReq.on('error', function (e) {
+        res.status(200).send(e);
+    });
 });
 
 // Start the server
